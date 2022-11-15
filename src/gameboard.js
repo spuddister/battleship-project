f81de1bc5897aababa632e4646c3420b7403ce7e
Sam Puddister <sampuddister@gmail.com>
2022-11-15 16:03:51 -0500

    Added gameboard function to check if all ships are sunk.

--- src/gameboard.js
+++ src/gameboard.js
@@ -17,10 +17,10 @@ function gameboardBuilder() {
     [, , , , , , , , , ,],
   ];
 
-  //temporarily assigning ship locations for codings sake
-  const tempShipLocations = [];
+  //temporarily hard code assigning ship locations for codings sake
+  const shipLocations = [];
   for (let i = 0; i < 10; i++) {
-    tempShipLocations[i] = [
+    shipLocations[i] = [
       [i, 0],
       [i, defaultShipLengths[i] - 1],
     ];
@@ -28,8 +28,8 @@ function gameboardBuilder() {
 
   //create ships and assign them coordinates from the temp locations
   //update the seaLayout to track where all ships are located
-  for (let i = 0; i < tempShipLocations.length; i++) {
-    playerShips[i] = ship(tempShipLocations[i][0], tempShipLocations[i][1]);
+  for (let i = 0; i < shipLocations.length; i++) {
+    playerShips[i] = ship(shipLocations[i][0], shipLocations[i][1]);
     playerShips[i].coordinates.forEach((coord) => {
       seaLayout[coord[0]][coord[1]] = playerShips[i];
     });
@@ -37,34 +37,25 @@ function gameboardBuilder() {
 
   const receiveAttack = (x, y) => {
     if (seaLayout[x][y] !== undefined) {
-      let vessel = seaLayout[x][y];
-      vessel.hit();
+      seaLayout[x][y].hit();
       seaLayout[x][y] = "hit";
-      if (vessel.isSunk()) sunkenShip(vessel);
       return "hit";
     } else {
       return "miss";
     }
   };
 
-  const indentifyShipAt = (x, y) => {
-    for (let k = 0; k < playerShips.length; k++) {
-      let coords = playerShips[k].coordinates;
-      for (let i = 0; i < coords.length; i++) {
-        if (coords[i][0] === x && coords[i][1] === y) {
-          return playerShips[k];
-        }
-      }
+  const allShipsSunk = () => {
+    for (let i = 0; i < playerShips.length; i++) {
+      if (!playerShips[i].isSunk()) return false;
     }
-  };
-
-  const sunkenShip = (vessel) => {
-    console.log("Sunk: " + vessel.coordinates);
+    return true;
   };
 
   return {
     playerShips,
     receiveAttack,
+    allShipsSunk,
   };
 }
 
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -19,3 +19,29 @@ it("Receiving attack test", () => {
   expect(gameboard.receiveAttack(9, 0)).toStrictEqual("hit");
   expect(gameboard.receiveAttack(0, 4)).toStrictEqual("miss");
 });
+
+it("Are all ships sunk?", () => {
+  const gameboard = gameboardBuilder();
+  gameboard.receiveAttack(...gameboard.playerShips[0].coordinates[0]);
+  gameboard.receiveAttack(...gameboard.playerShips[0].coordinates[1]);
+  gameboard.receiveAttack(...gameboard.playerShips[0].coordinates[2]);
+  gameboard.receiveAttack(...gameboard.playerShips[0].coordinates[3]);
+  gameboard.receiveAttack(...gameboard.playerShips[1].coordinates[0]);
+  gameboard.receiveAttack(...gameboard.playerShips[1].coordinates[1]);
+  gameboard.receiveAttack(...gameboard.playerShips[1].coordinates[2]);
+  gameboard.receiveAttack(...gameboard.playerShips[2].coordinates[0]);
+  gameboard.receiveAttack(...gameboard.playerShips[2].coordinates[1]);
+  gameboard.receiveAttack(...gameboard.playerShips[2].coordinates[2]);
+  gameboard.receiveAttack(...gameboard.playerShips[3].coordinates[0]);
+  gameboard.receiveAttack(...gameboard.playerShips[3].coordinates[1]);
+  gameboard.receiveAttack(...gameboard.playerShips[4].coordinates[0]);
+  gameboard.receiveAttack(...gameboard.playerShips[4].coordinates[1]);
+  gameboard.receiveAttack(...gameboard.playerShips[5].coordinates[0]);
+  gameboard.receiveAttack(...gameboard.playerShips[5].coordinates[1]);
+  gameboard.receiveAttack(...gameboard.playerShips[6].coordinates[0]);
+  gameboard.receiveAttack(...gameboard.playerShips[7].coordinates[0]);
+  gameboard.receiveAttack(...gameboard.playerShips[8].coordinates[0]);
+  expect(gameboard.allShipsSunk()).toStrictEqual(false);
+  gameboard.receiveAttack(...gameboard.playerShips[9].coordinates[0]);
+  expect(gameboard.allShipsSunk()).toStrictEqual(true);
+});
