2134a6eba9f4e6f2753b8e829dbfb127bbed13e5
Sam Puddister <sampuddister@gmail.com>
2022-11-23 15:31:41 -0500

    Work on integrating the game into the interface in progress. Design of gameflow decided on and first steps built.

--- src/index.js
+++ src/index.js
@@ -5,23 +5,55 @@ import "./style.css";
 
 const playerBuilder = require("./player");
 
-const players = playerBuilder();
+let players;
 
-const gameLoop = () => {};
+//Initial HMTL Framework
+const mainDiv = document.createElement("div");
+mainDiv.setAttribute("id", "main");
+const headerDiv = document.createElement("header");
+const boardsDiv = document.createElement("div");
+boardsDiv.classList.add("board-wrapper");
 
-const gameFramework = () => {
-  const mainDiv = document.createElement("div");
-  mainDiv.setAttribute("id", "main");
-  const headerDiv = document.createElement("header");
+const startBtnDiv = document.createElement("div");
+startBtnDiv.classList.add("start-btn-wrapper");
+const startBtn = document.createElement("button");
+startBtn.setAttribute("id", "start-btn");
+startBtn.textContent = "Start Battle";
+startBtn.addEventListener("click", () => {
+  startBtn.classList.add("start-hidden");
+  buildGameHMTL();
+  players = playerBuilder();
+  startBtn.disabled = true;
+});
 
-  const startBtnDiv = document.createElement("div");
-  startBtnDiv.classList.add("start-btn-wrapper");
-  const startBtn = document.createElement("button");
-  startBtn.setAttribute("id", "start-btn");
-  startBtn.textContent = "Start Battle";
+mainDiv.appendChild(headerDiv);
+startBtnDiv.appendChild(startBtn);
+mainDiv.appendChild(startBtnDiv);
+mainDiv.appendChild(boardsDiv);
+document.body.appendChild(mainDiv);
 
-  const boardsDiv = document.createElement("div");
-  boardsDiv.classList.add("board-wrapper");
+const gameLoop = (e) => {
+  //event listnr on tiles push game into next step, checks if computer ships are all sunk and then calls computer to make a turn and then checks if players ships are sunk. player clicks new tile and process continues
+
+  //Get coordinates of the tile clicked from the tile-number, tile#96 has coordinates [6,9]
+  let tileNum = e.target.getAttribute("tile-num");
+  let x;
+  let y;
+  if (tileNum.length === 1) {
+    y = 0;
+    x = tileNum;
+  } else {
+    y = tileNum[0];
+    x = tileNum[1];
+  }
+  //call attack with coordinates and update tile to reflect change
+  e.target.classList.add(players.player.attack(x, y));
+  e.target.removeEventListener("click", gameLoop);
+  e.target.classList.remove("clickable-tile");
+  // e.target.classList.add("miss");
+};
+
+const buildGameHMTL = () => {
   const playerBoardDiv = document.createElement("div");
   playerBoardDiv.classList.add("board");
   const compBoardDiv = document.createElement("div");
@@ -38,33 +70,14 @@ const gameFramework = () => {
     playerTiles[i].classList.add("tile");
     compTiles[i].classList.add("tile");
     playerTiles[i].classList.add("player-tile");
-    compTiles[i].classList.add("comp-tile");
-    compTiles[i].addEventListener("click", (e) => {
-      let tileNum = e.target.getAttribute("tile-num");
-      let x;
-      let y;
-      if (tileNum.length === 1) {
-        y = 0;
-        x = tileNum;
-      } else {
-        y = tileNum[0];
-        x = tileNum[1];
-      }
-      e.target.classList.add(players.player.attack(x, y));
-      // e.target.classList.add("miss");
-    });
+    compTiles[i].classList.add("clickable-tile");
+    //These event listeners on each tile drive the progression of the game, each time one is clicked, the program moves forward until user input is required again
+    compTiles[i].addEventListener("click", gameLoop);
     playerBoardDiv.appendChild(playerTiles[i]);
     compBoardDiv.appendChild(compTiles[i]);
     i++;
   }
 
-  mainDiv.appendChild(headerDiv);
-  startBtnDiv.appendChild(startBtn);
-  mainDiv.appendChild(startBtnDiv);
-  mainDiv.appendChild(boardsDiv);
   boardsDiv.appendChild(playerBoardDiv);
   boardsDiv.appendChild(compBoardDiv);
-  document.body.appendChild(mainDiv);
 };
-
-gameFramework();
